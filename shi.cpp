52bb7eb747e2d92f5c8f5c428895b8e29eeabd33
心夏~ <219548426+xinxia072@users.noreply.github.com>
2025-07-09 16:00:29 +0800

    Update shi.cpp

--- shi.cpp
+++ shi.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 typedef void(__stdcall *set_fps_t)(int fps);
-void shit() {
+void shit(HMODULE hModule) {
     //AllocConsole();
     //FILE* pConsole;
     //freopen_s(&pConsole, "CONOUT$", "w", stdout);
@@ -15,18 +15,25 @@ void shit() {
         Sleep(100);
     }
     //printf("found gameassembly.dl;\n");
-    set_fps_t set_fps = (set_fps_t)(game_assembly + 0x763CD40);
 
-    while (1) {
-        set_fps(-1);
-        Sleep(5000);
-    }
+    uint64_t set_fps_offset = game_assembly + 0x763CD60; // UnityEngine.Application.set_targetFrameRate
+    set_fps_t set_fps = (set_fps_t)(set_fps_offset);
+    set_fps(240);
+
+    // prevent game setting fps limit again
+    DWORD oldProtect;
+    VirtualProtect(reinterpret_cast<LPVOID>(set_fps_offset), 1, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *reinterpret_cast<BYTE*>(set_fps_offset) = 0xC3; //old is 0x56
+    VirtualProtect(reinterpret_cast<LPVOID>(set_fps_offset), 1, oldProtect, &oldProtect);
+
+    // unload dll
+    FreeLibraryAndExitThread(hModule, 0);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         // meow cutely (permanent banned)
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)shit, NULL, 0, NULL);
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)shit, hModule, 0, NULL);
     }
     return 1;
 }
